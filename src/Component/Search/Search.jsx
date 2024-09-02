94004386c985589de69a7fdb27d54a31bf985b79
felo <felo.safwat2000@gmail.com>
2024-09-03 01:41:34 +0300

    Done

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -58,7 +58,7 @@ export default function Search() {
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
-                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Movie Name" onChange={(e) => { searchinput(e.target.value) }} />
+                    <input type="text" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Movie Name" onChange={(e) => { searchinput(e.target.value) }} />
 
                 </div>
             </form>
